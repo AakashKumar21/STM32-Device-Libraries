eebc9af61bd7f1700a7316c809eb5c266040ca41
Aakash <aakashk.kvjp58@gmail.com>
2021-01-09 23:26:24 +0530

    Update dht11.h

--- Inc/dht11.h
+++ Inc/dht11.h
@@ -2,6 +2,8 @@
   * @file           : dht11.h
   * @brief          : Header for dht11.c file.
 **/
+#ifndef __DHT11_H
+#define __DHT11_H
 
 #include "main.h"
 #include "main.h"
@@ -25,6 +27,4 @@ static uint8_t _read_byte(GPIO_TypeDef* port, uint16_t pin);
 DHT11_Status DHT11_read(GPIO_TypeDef* port, uint16_t pin, 
                         uint8_t* temp, uint8_t *hum);
 
-#ifndef __DHT11_H
-#define __DHT11_H
 #endif
